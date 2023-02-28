95367dba4c36e68b274fafac658ce1d4dbe08889
Saul Lee <solleedata@gmail.com>
2023-02-28 12:50:36 +0900

    feat: LocalStrategy (user logged in or out case)

--- routes/page.js
+++ routes/page.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 
 const router = express.Router();
 
 router.use((req, res, next) => {
+  res.locals.user = req.user;
   res.locals.user = null;
   res.locals.followerCount = 0;
   res.locals.followingCount = 0;
@@ -10,10 +12,12 @@ router.use((req, res, next) => {
   next();
 });
 
+// router.get("/profile", isLoggedIn, renderProfile);
 router.get("/profile", (req, res) => {
   res.render("profile", { title: "내 정보 - NodeBird" });
 });
 
+// router.get("/join", isNotLoggedIn, renderJoin);
 router.get("/join", (req, res) => {
   res.render("join", { title: "회원가입 - NodeBird" });
 });
